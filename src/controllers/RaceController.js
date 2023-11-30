4fb6de3148ddc247d3e9a4c950fc3be7adb21185
yunseorim1116 <hhyjs2r@gmail.com>
2023-11-30 13:57:53 +0900

    [feat] 자동차 경주를 controller 하는 controller를 생성한다.

---
+++ src/controllers/RaceController.js
@@ -0,0 +1,11 @@
+import { Random, Console } from '@woowacourse/mission-utils';
+import InputView from '../views/InputView.js';
+import CarNameValidator from '../validator/CarNameValidator.js';
+
+class RaceController {
+  constructor() {}
+
+  async start() {}
+}
+
+export default RaceController;
