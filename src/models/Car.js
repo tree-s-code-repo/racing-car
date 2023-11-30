a01d5a1076f2238d99b792583a53ee05625a92a1
yunseorim1116 <hhyjs2r@gmail.com>
2023-11-30 14:11:08 +0900

    [feat] 자동차 모델을 생성한다.

---
+++ src/models/Car.js
@@ -0,0 +1,23 @@
+import { Random } from '@woowacourse/mission-utils';
+
+class Car {
+  #name;
+  #position;
+  #MINIMUM_MOVE_VALUE = 4;
+
+  constructor(name) {
+    this.#name = name;
+    this.#position = 0;
+  }
+
+  move() {
+    const randomNumber = Random.pickNumberInRange(0, 9);
+    if (randomNumber >= this.#MINIMUM_MOVE_VALUE) {
+      this.#setPosition();
+    }
+  }
+  #setPosition() {
+    this.#position += 1;
+  }
+}
+export default Car;
